15c363aa5f60a20c96070ce84b3e52175208cabe
Alex Johnston <alex-johnston@live.co.uk>
2024-03-22 15:30:29 +0000

    Initial commit

---
+++ test/portfolio-aws-cdk-vpc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PortfolioAwsCdkVpc from '../lib/portfolio-aws-cdk-vpc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/portfolio-aws-cdk-vpc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PortfolioAwsCdkVpc.PortfolioAwsCdkVpcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
