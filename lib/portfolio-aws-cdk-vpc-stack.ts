051c8926da036ba4bc5f92829ce3838bb4605736
Alex Johnston <alex-johnston@live.co.uk>
2024-03-22 16:27:03 +0000

    Adding ESLint config and stack information

--- lib/portfolio-aws-cdk-vpc-stack.ts
+++ lib/portfolio-aws-cdk-vpc-stack.ts
@@ -1,16 +1,32 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CfnOutput, Stack, type StackProps } from 'aws-cdk-lib'
+import { type Construct } from 'constructs'
+import { Vpc } from 'aws-cdk-lib/aws-ec2'
 
-export class PortfolioAwsCdkVpcStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class PortfolioAwsCdkVpcStack extends Stack {
+  constructor (scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, 'PortfolioVpc', {
+      vpcName: 'Portfolio',
+      maxAzs: 3
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PortfolioAwsCdkVpcQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CfnOutput(this, 'PortfolioVpcId', {
+      exportName: 'Portfolio-Vpc-Id',
+      value: vpc.vpcId
+    })
+
+    const publicSubnetIds = vpc.publicSubnets.map(subnet => subnet.subnetId)
+    const privateSubnetIds = vpc.privateSubnets.map(subnet => subnet.subnetId)
+
+    new CfnOutput(this, 'PortfolioVpcPublicSubnetIds', {
+      value: publicSubnetIds.join(','),
+      exportName: 'Portfolio-Vpc-Public-Subnet-Ids'
+    })
+
+    new CfnOutput(this, 'PortfolioVpcPrivateSubnetIds', {
+      value: privateSubnetIds.join(','),
+      exportName: 'Portfolio-Vpc-Private-Subnet-Ids'
+    })
   }
 }
